ae9693229053160ec8fac0e581a01be9e40ec1be
sameerthe8055 <csamber61@gmail.com>
2025-06-09 18:37:25 +0530

    add gcd and lcd notes taken from online-class@26-05-2025

---
+++ online_class@26-05-2025.py
@@ -0,0 +1,15 @@
+
+## gcd
+def gcd(n1, n2):
+    while(n2 != n1):
+        if n1 > n2:
+            n1 = n1 - n2
+        else:
+            n2 = n2 - n1
+    return n1
+
+## lcm = (n1 * n2) // gcd(n1, n2)
+def lcm (n1, n2):
+    return (n1 * n2) // gcd(n1, n2)
+
+print(lcm(2, 3))
